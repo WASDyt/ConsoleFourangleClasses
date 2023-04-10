d85eabbf2cb5d5a344cb779f00937befc38e69e2
WASDyt <wasd50983@gmail.com>
2023-04-10 13:14:18 +0300

    Добавьте файлы проекта.

---
+++ Classes/ClsParallelogram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFourangleClasses.Classes
+{
+    internal class ClsParallelogram: ClsFourangle
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleFourangleClasses.Classes;
+
+Console.WriteLine("Чектырехугольник1");
+ClsFourangle fourngle1 = new ClsFourangle();
+fourngle1.KeyboardInput();
+fourngle1.SidesCalculation();
+fourngle1.DiagonalsCalculation();
+Console.WriteLine($"Периметр = {fourngle1.Perimetr()}");
+
+Console.WriteLine("Четырехугольник 2");
+ClsFourangle fourngle2 = new ClsFourangle(-5, 4, 8, 6, 9, -4, -4, -3);
+
+fourngle2.SidesCalculation();
+fourngle2.DiagonalsCalculation();
+Console.WriteLine($"Периметр = {fourngle2.Perimetr()}");
